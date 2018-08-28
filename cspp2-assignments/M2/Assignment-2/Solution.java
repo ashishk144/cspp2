9b20780a7d6521e7cd391dd268a55c470a777c04
ashishk144 <ashishk1404@msitprogram.net>
2018-08-28 17:07:52 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-2/Solution.java
+++ cspp2-assignments/M2/Assignment-2/Solution.java
@@ -13,30 +13,32 @@ public class Solution {
          * Main Function
          */
         Scanner scan = new Scanner(System.in);
-        final int a = scan.nextInt();
-        final int b = scan.nextInt();
-        final int c = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
         rootsOfQuadraticEquation(a, b, c);
     }
-    static void rootsOfQuadraticEquation(int a, int b, int c) {
     /**
      * Roots of quadratic equation function
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
      */
-    /*
-    Need to write the rootsOfQuadraticEquation function and print the output.
-    */
+    static void rootsOfQuadraticEquation(final int a,final int b,final int c) {
     // System.out.println("abc");
     // System.out.println(b*b);
     // System.out.println(((b*b)-(4*a*c)));
-    if (((b * b) - (4 * a * c)) >= 0) {
-    double root1 = (-b + Math.sqrt(((b * b) - (4 * a * c)))) / (2 * a);
-    double root2 = (-b - Math.sqrt(((b * b) - (4 * a * c)))) / (2 * a);
+    int four = 4;
+    if (((b * b) - (four * a * c)) >= 0) {
+    double root1 = (-b + Math.sqrt(((b * b) - (four * a * c)))) / (2 * a);
+    double root2 = (-b - Math.sqrt(((b * b) - (four * a * c)))) / (2 * a);
     System.out.println(root1 + " " + root2);
     // System.out.println(root_2);
     }
     // else{
-    //  double root_1_sqrt = Math.sqrt(Math.abs((b*b)-(4*a*c)));
-    //  // double root_2_sqrt = Math.sqrt(abs((b*b)-(4*a*c)));
+    //  double root_1_sqrt = Math.sqrt(Math.abs((b*b)-(four*a*c)));
+    //  // double root_2_sqrt = Math.sqrt(abs((b*b)-(four*a*c)));
     //  System.out.println(-b + "/" + 2*a + "+i" + root_1_sqrt + "/" + 2*a);
     //  System.out.println(-b + "/" + 2*a + "-i" + root_1_sqrt + "/" + 2*a);
     // }
