fc26fe0533e2df131dd9691776f709e76329e92e
ashishk144 <ashishk1404@msitprogram.net>
2018-08-28 14:43:29 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-2/Solution.java
+++ cspp2-assignments/M2/Assignment-2/Solution.java
@@ -11,16 +11,16 @@ public class Solution {
         int c = scan.nextInt();
         rootsOfQuadraticEquation(a, b, c);
     }
-    static void rootsOfQuadraticEquation(int a, int b, int c){
+    static void rootsOfQuadraticEquation(int a, int b, int c) {
     /*
     Need to write the rootsOfQuadraticEquation function and print the output.
     */
     // System.out.println("abc");
     // System.out.println(b*b);
     // System.out.println(((b*b)-(4*a*c)));
-    if (((b * b)-(4 * a * c)) >= 0){
-    double root1 = (-b + Math.sqrt(((b * b) - (4 * a * c))))/(2 * a);
-    double root2 = (-b - Math.sqrt(((b * b) - (4 * a * c))))/(2 * a);
+    if (((b * b) - (4 * a * c)) >= 0) {
+    double root1 = (-b + Math.sqrt(((b * b) - (4 * a * c)))) / (2 * a);
+    double root2 = (-b - Math.sqrt(((b * b) - (4 * a * c)))) / (2 * a);
     System.out.println(root1 + " " + root2);
     // System.out.println(root_2);
     }
