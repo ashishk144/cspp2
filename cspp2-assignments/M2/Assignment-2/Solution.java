74a09ae0a1a7671f5833e1035c56631991d467df
ashishk144 <ashishk1404@msitprogram.net>
2018-08-28 14:47:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-2/Solution.java
+++ cspp2-assignments/M2/Assignment-2/Solution.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
+/*
+Writing a program for finding square roots of quadratic equations*/
 public class Solution {
     /*
     Do not modify this main function.
