a710f6fcbc707406d2e7d3b8fff0e920988ac309
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 13:43:57 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-2/Solution.java
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -24,25 +24,25 @@ final class Solution {
 				a[i][j] = sc.nextInt();
 			}
 		}
-		int m_1 = sc.nextInt(), n_1 = sc.nextInt();
-		int[][] b = new int[m_1][n_1];
-		for (int i = 0; i < m_1; i++) {
-			for (int j = 0; j < n_1; j++) {
+		int m1 = sc.nextInt(), n1 = sc.nextInt();
+		int[][] b = new int[m1][n1];
+		for (int i = 0; i < m1; i++) {
+			for (int j = 0; j < n1; j++) {
 				b[i][j] = sc.nextInt();
 			}
 		}
-		if (m_1 != m || n_1 != n) {
+		if (m1 != m || n1 != n) {
 			System.out.println("not possible");
 		} else {
-			int[][] c = new int[m_1][n_1];
-		for (int i = 0; i < m_1; i++) {
-			for (int j = 0; j < n_1; j++) {
+			int[][] c = new int[m1][n1];
+		for (int i = 0; i < m1; i++) {
+			for (int j = 0; j < n1; j++) {
 				c[i][j] = a[i][j] + b[i][j];
             }
 		}
         int i = 0, j=0;
-        for (; i < m_1; i++) {
-            for (; j < n_1-1; j++) {
+        for (i = 0; i < m1; i++) {
+            for (j = 0; j < n1-1; j++) {
                 System.out.print(c[i][j] + " ");
             }
         System.out.print(c[i][j]);
