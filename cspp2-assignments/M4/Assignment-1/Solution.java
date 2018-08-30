42a55db4fe5fa10643d6b7a2c0d7528c968db6b5
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 12:04:55 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/M4/Assignment-1/Solution.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Main function
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max;
+        if (n < 1) {
+            System.out.println("Array size cannot be less than 1");
+        } else {
+            int [] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            max = a[0];
+            for (int i = 0; i < n; i++) {
+                if (max < a[i]) {
+                    max = a[i];
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
