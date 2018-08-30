36b840575c1469cb11791f1483c50f17c789dd26
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 13:41:36 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-2/Solution.java
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -4,47 +4,50 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-    /**
-     * Default constructor
-     */
-    private Solution() {
+	/**
+	 * Default constructor
+	 */
+	private Solution() {
 
-    }
-    /**.
-     * Main function
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt(), n = sc.nextInt();
-        int[][] a = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = sc.nextInt();
+	}
+	/**.
+	 * Main function
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt(), n = sc.nextInt();
+		int[][] a = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
+		int m_1 = sc.nextInt(), n_1 = sc.nextInt();
+		int[][] b = new int[m_1][n_1];
+		for (int i = 0; i < m_1; i++) {
+			for (int j = 0; j < n_1; j++) {
+				b[i][j] = sc.nextInt();
+			}
+		}
+		if (m_1 != m || n_1 != n) {
+			System.out.println("not possible");
+		} else {
+			int[][] c = new int[m_1][n_1];
+		for (int i = 0; i < m_1; i++) {
+			for (int j = 0; j < n_1; j++) {
+				c[i][j] = a[i][j] + b[i][j];
             }
-        }
-        int m_1 = sc.nextInt(), n_1 = sc.nextInt();
-        int[][] b = new int[m_1][n_1];
-        for (int i = 0; i < m_1; i++) {
-            for (int j = 0; j < n_1; j++) {
-                b[i][j] = sc.nextInt();
+		}
+        int i = 0, j=0;
+        for (; i < m_1; i++) {
+            for (; j < n_1-1; j++) {
+                System.out.print(c[i][j] + " ");
             }
+        System.out.print(c[i][j]);
+        System.out.println();
         }
-        if (m_1 != m || n_1 != n) {
-            System.out.println("not possible");
-        } else {
-            // int[][] c = new int[m_1][n_1];
-            int c = 0;
-            for (int i = 0; i < m_1; i++) {
-                for (int j = 0; j < n_1; j++) {
-                    c = a[i][j] + b[i][j];
-                    System.out.print(c + " ");
-                } System.out.println();
-            }
         }
-        // for (int element : c) {
-            
-        // }
-    }
+	}
 }
