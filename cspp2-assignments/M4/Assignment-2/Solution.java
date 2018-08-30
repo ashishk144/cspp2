e4808f41064f6a3c591b9288cfa2cec2df5df2f2
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 12:31:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Default constructor
+	 */
+	private Solution() {
+
+	}
+	/**.
+	 * Main function
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt(), n = sc.nextInt();
+		int[][] a = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		System.out.println(Arrays.toString(a));
+		}
+
+	}
+}
