cfcfa03c1da43f213aff72cccd0d5b719c028415
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 13:34:27 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-2/Solution.java
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -4,27 +4,47 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Default constructor
-	 */
-	private Solution() {
+    /**
+     * Default constructor
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * Main function
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt(), n = sc.nextInt();
-		int[][] a = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = sc.nextInt();
-				System.out.println(a[i][j]);
-			}
-		}
-
-	}
+    }
+    /**.
+     * Main function
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int m_1 = sc.nextInt(), n_1 = sc.nextInt();
+        int[][] b = new int[m_1][n_1];
+        for (int i = 0; i < m_1; i++) {
+            for (int j = 0; j < n_1; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        if (m_1 != m || n_1 != n) {
+            System.out.println("not possible");
+        } else {
+            // int[][] c = new int[m_1][n_1];
+            int c = 0;
+            for (int i = 0; i < m_1; i++) {
+                for (int j = 0; j < n_1; j++) {
+                    c = a[i][j] + b[i][j];
+                    System.out.print(c + " ");
+                } System.out.println();
+            }
+        }
+        // for (int element : c) {
+            
+        // }
+    }
 }
