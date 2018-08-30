53a0e05a7a9d502782746a6602a4406fa7830588
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 13:44:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-2/Solution.java
+++ cspp2-assignments/M4/Assignment-2/Solution.java
@@ -4,50 +4,50 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Default constructor
-	 */
-	private Solution() {
+    /**.
+     * Default constructor
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * Main function
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt(), n = sc.nextInt();
-		int[][] a = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		int m1 = sc.nextInt(), n1 = sc.nextInt();
-		int[][] b = new int[m1][n1];
-		for (int i = 0; i < m1; i++) {
-			for (int j = 0; j < n1; j++) {
-				b[i][j] = sc.nextInt();
-			}
-		}
-		if (m1 != m || n1 != n) {
-			System.out.println("not possible");
-		} else {
-			int[][] c = new int[m1][n1];
-		for (int i = 0; i < m1; i++) {
-			for (int j = 0; j < n1; j++) {
-				c[i][j] = a[i][j] + b[i][j];
+    }
+    /**.
+     * Main function
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt(), n = sc.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
             }
-		}
-        int i = 0, j=0;
+        }
+        int m1 = sc.nextInt(), n1 = sc.nextInt();
+        int[][] b = new int[m1][n1];
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        if (m1 != m || n1 != n) {
+            System.out.println("not possible");
+        } else {
+            int[][] c = new int[m1][n1];
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        int i = 0, j = 0;
         for (i = 0; i < m1; i++) {
-            for (j = 0; j < n1-1; j++) {
+            for (j = 0; j < n1 - 1; j++) {
                 System.out.print(c[i][j] + " ");
             }
         System.out.print(c[i][j]);
         System.out.println();
         }
         }
-	}
+    }
 }
