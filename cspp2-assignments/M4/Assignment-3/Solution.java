75f82764c0523ea9584e947c44865993e9d052fe
ashishk144 <ashishk1404@msitprogram.net>
2018-08-30 14:34:44 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-3/Solution.java
+++ cspp2-assignments/M4/Assignment-3/Solution.java
@@ -1,5 +1,6 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+// Given an String, Write a java method that returns the decimal value for the
+//given binary string.
 
 
 import java.util.Scanner;
@@ -13,8 +14,7 @@ final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
