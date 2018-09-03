b4b2333eb2d7a608245eea1dd59a0eec658dd29d
ashishk144 <ashishk1404@msitprogram.net>
2018-09-03 17:18:38 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M7/Assignment-1/Solution.java
+++ cspp2-assignments/M7/Assignment-1/Solution.java
@@ -1,19 +1,29 @@
-import java.util.*;
-/**
+import java.util.Scanner;
+/**.
  * Class for input validation.
  */
 class InputValidator {
     String inp;
+    /**
+     * Constructs the object.
+     *
+     * @param      startinp  The startinp
+     */
     public InputValidator(String startinp) {
-        String inp = startinp;
+        inp = startinp;
     }
-    // public int length() {
-    //     int len = 0;
-    //     for (InputValidator eachval: inp) {
-    //         len += 1;
-    //     }
-    // return len;
-    // }
+    public int length() {
+        int len = 0;
+        for (char eachval:inp.toCharArray()) {
+            len += 1;
+        }
+    return len;
+    }
+    /**
+     * Validation method
+     *
+     * @return     { Boolean }
+     */
     public boolean validateData() {
         if (inp.length() >= 5) {
             return true;
@@ -22,17 +32,19 @@ class InputValidator {
         }
     }
 }
-/**
+/**.
  * Class for solution.
  */
 public class Solution {
-    /**
+    /**.
      * Main function
+     *
+     * @param      args  The arguments
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = scan.next();
-        InputValidator i = new InputValidator(input);
-        System.out.println(i.validateData());
+        InputValidator inp = new InputValidator(input);
+        System.out.println(inp.validateData());
     }
 }
