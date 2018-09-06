9c5ee6b22838adfba44c392b81ec970c834ba01a
ashishk144 <ashishk1404@msitprogram.net>
2018-09-06 10:55:42 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M10/Assignment-2/StringList.java
+++ cspp2-assignments/M10/Assignment-2/StringList.java
@@ -165,7 +165,7 @@ public class StringList implements StringListInterface{
      */
 
     public void remove(int index) {
-        if (index < 0 && index > size) {
+        if (index < 0) {
             System.out.println("Negative Index Exception");
         } else {
         for (int i = index; i < size - 1; i++) {
