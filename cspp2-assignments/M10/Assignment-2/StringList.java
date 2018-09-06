695d201e60f8364b2868d5b1b7f791dc00204de4
ashishk144 <ashishk1404@msitprogram.net>
2018-09-06 11:18:36 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M10/Assignment-2/StringList.java
+++ cspp2-assignments/M10/Assignment-2/StringList.java
@@ -171,7 +171,7 @@ public class StringList implements StringListInterface{
      */
 
     public void remove(int index) {
-        if (index < 0 && index >= size) {
+        if (index > size & index < 0 ) {
             System.out.println("Invalid Position Exception");
         } else {
         for (int i = index; i < size - 1; i++) {
