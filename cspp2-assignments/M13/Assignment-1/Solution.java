9829caa5e797a477ae617a59db4c2b994df18f9f
ashishk144 <ashishk1404@msitprogram.net>
2018-09-08 11:36:02 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M13/Assignment-1/Solution.java
+++ cspp2-assignments/M13/Assignment-1/Solution.java
@@ -159,10 +159,10 @@ class Set {
     public int[][] cartesianProduct(Set inpset) {
         if (size > 0 && inpset.size() > 0) {
             int[][] newarray = new int[size * inpset.size()][2];
-            for (int k = 0; k < size * inpset.size(); k++) {
+            for (int i = 0; i < size; i++) {
                 for (int j = 0; j < (inpset.size()); j++) {
-                    newarray[k][0] = arr[k];
-                    newarray[k][j] = inpset.get(j);
+                    newarray[i][0] = arr[i];
+                    newarray[i][j] = inpset.get(j);
                 }
             }
         return newarray;
