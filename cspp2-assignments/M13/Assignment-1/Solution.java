164d3f8cb729c2c61249d7a13bab41da69edacc1
ashishk144 <ashishk1404@msitprogram.net>
2018-09-08 11:32:06 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M13/Assignment-1/Solution.java
+++ cspp2-assignments/M13/Assignment-1/Solution.java
@@ -161,7 +161,7 @@ class Set {
             int[][] newarray = new int[size * inpset.size()][2];
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < (inpset.size() - 1); j++) {
-                    newarray[j][0] = arr[i];
+                    newarray[i][0] = arr[i];
                     newarray[i][j] = inpset.get(j);
                 }
             }
